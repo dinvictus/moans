562a94e139622f1aafc59da4cadc58989c226bdc
dinvictus <dmitry.deev@bk.ru>
2022-03-07 17:25:28 +0300

    add shimmer effect to feed

---
+++ lib/actionconfirm.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ActionConfirm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/back/back.png'), fit: BoxFit.fill)),
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
